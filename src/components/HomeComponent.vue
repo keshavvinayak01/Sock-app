1919bfe54b07b0416f6225a48a656eecb4e7e081
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-05 17:50:17 +0530

    Initialised HomeComponent

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="product">
+    <div class="product-image">
+      <img v-bind:src="image" />
+    </div>
+    <div class="product-info">
+      <h1>{{ product }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    product: String,
+  },
+  data : function() {
+    return {
+      image : "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
