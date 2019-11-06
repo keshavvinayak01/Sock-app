f9ac5632acbad21dc78dae7b71c1c81067d923e7
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-11-06 18:26:01 +0530

    Initialized product-tabs

---
+++ src/components/ProductTabsComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <span 
+      class="tab"
+      v-for="(tab, index) in tabs" :key="index" >
+        {{ tab }}
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductTabsComponent',
+  components: {
+  },
+  data() {
+    return {
+        tabs : ['Reviews', 'Make a review']
+    }
+  },
+  methods : {
+  }
+}
+</script>
